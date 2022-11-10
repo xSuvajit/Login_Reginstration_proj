aef52e640e979cdd5551f493179e5d77a5cd56f1
suvajit97 <iamsuvajitsarkar@gmail.com>
2022-11-10 11:54:36 +0530

    return to initial state

--- Login_Reginstration_proj/Controllers/ValuesController.cs
+++ Login_Reginstration_proj/Controllers/ValuesController.cs
@@ -6,40 +6,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Login_Reginstration_proj.Models;
 
 namespace Login_Reginstration_proj.Controllers
 {
 
     public class ValuesController : ApiController
     {
-        private HCLEntities obj = new HCLEntities();
-
-        //string cs = @"data source=STAR_DUST\SQLEXPRESS;initial catalog=HCL;integrated security=True";
-        //string cons = getConString();
-
-        private SqlConnection con = new SqlConnection(getConString());
-
-        private static string getConString()
-        {
-            using (var streamReader = File.OpenText(@"C:\Connection.txt"))
-            {
-                var lines = streamReader.ReadToEnd();
-                return lines;
-            }
-        }
-
         // GET api/values
-        public IEnumerable<Employee> Get()
-        {
-            //using (SqlConnection con = new SqlConnection(cons))
-            //{
-            //    SqlCommand sql = new SqlCommand("select * from Employees", con);
-            //    con.Open();
-            //}
-
-            con.Open();
-            return obj.Employees.ToList ();
+        public IEnumerable<String> Get()
+        {            
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
